c433aa9dfacc6241c17bc0eccc11f950aa045134
MicaViera <vieranoelia98@gmail.com>
2023-08-18 22:05:03 +0000

    Function that gets the PATH, finds the executable PATH and prints the enviroment.

---
+++ enveneitor.c
@@ -0,0 +1,59 @@
+#include "main.h"
+/**
+ * patheitor - Function that gets the PATH.
+ * @fpath: The name of the enviroment variable to find.
+ * return: The PATH.
+*/
+char *patheitor(const char *fpath)
+{
+	size_t lenght = strlen(fpath);
+	int i = 0;
+	char *current_env = NULL;
+
+	for (; environ[i] != NULL; i++)
+	{
+		current_env = environ[i];
+
+		if (strncmp(current_env, fpath, lenght) == 0 && current_env[lenght] == '=')
+			return (&current_env[lenght + 1]);
+	}
+	return (NULL);
+}
+
+/**
+ * findex_path - Function that finds the executable path of the given command.
+ * @command: The command.
+ * @path: The PATH.
+ * return: Apointer with the executable PATH.
+*/
+char *findex_path(const char *command, const char *path)
+{
+	char *copy_path = strdup(path);
+	char *token = strtok(copy_path, ":");
+	char commandpath[1024];
+
+	while (copy_path != NULL)
+	{
+		sprintf(commandpath, "%s/%s", token, command);
+		if (access(commandpath, X_OK) == 0)
+		{
+			free(copy_path);
+			return (strdup(commandpath));
+		}
+		token = strtok(NULL, ":");
+	}
+	free(copy_path);
+	return (NULL);
+}
+/**
+* enveneitor - function that prints the environment
+*/
+void enveneitor(void)
+{
+	char **penv;
+
+	for (penv = environ; *penv != NULL; penv++)
+	{
+		printf("%s\n", *penv);
+	}
+}
