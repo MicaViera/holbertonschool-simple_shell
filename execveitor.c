3602474a5e1d62d4fb54eab00aa7d4676088c2e5
MicaViera <vieranoelia98@gmail.com>
2023-08-18 22:02:56 +0000

    Function that executes and Forks.

---
+++ execveitor.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * execveitor - Function that executes.
+ * @comand: The comand.
+ * @argv: The arguments.
+ * return: The state of the system call execve.
+*/
+int execveitor(char *comand, char **argv)
+{
+	int state = 0;
+	char *path = patheitor("PATH");
+	char *pathito = NULL;
+	pid_t pid;
+	struct stat valid;
+
+	if (stat(comand, &valid) == 0)
+	{
+		pid = fork();
+		if (pid < 0)
+			perror("Error");
+		else if (pid == 0)
+		{
+			if (execve(comand, argv, environ) == -1)
+				perror("Error");
+		}
+		else
+			wait(&state);
+	}
+	else
+	{
+		pid = fork(), pathito = findex_path(argv[0], path);
+		if (pid == 0)
+		{
+			if (execve(pathito, argv, environ) == 1)
+			{
+				free(pathito);
+				perror("Error");
+				exit(1);
+			}
+		}
+		else
+			wait(&state);
+	}
+	return (WEXITSTATUS(state));
+}
